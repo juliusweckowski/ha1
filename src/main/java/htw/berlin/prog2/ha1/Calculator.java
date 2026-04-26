937d9cf1d4c4b850aa04db5cf3365ce1223632c3
Julius <Fenstersteuerungber SupportJulius.Weckowski@Student.HTW-Berlin.de>
2026-04-26 22:10:33 +0200

    fix equal key no operation

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -118,6 +118,9 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+        if(latestOperation.equals("")){
+            return;
+        }
         var result = switch(latestOperation) {
             case "+" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
